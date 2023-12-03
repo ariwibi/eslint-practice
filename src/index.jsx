a5ff2a2469f0d8d3d01d4d34090d1f3bf5bb59e7
ariwibi <ari.bowo.0231@gmail.com>
2023-12-03 22:49:27 +0800

    eslint-practice

---
+++ src/index.jsx
@@ -0,0 +1,9 @@
+import { createRoot } from "react-dom/client";
+import App from "./App";
+import React from "react";
+
+import "./styles/style.css";
+
+const root = createRoot(document.getElementById("root"));
+
+root.render(<App />);
